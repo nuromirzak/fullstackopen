c2ca2f50154a29f339aec2e06ec1c712b03137c2
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-06 21:08:29 +0600

    feat: done 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const getCount = (scores) => {
+    return scores.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  const getAverage = (scores) => {
+    const count = getCount(scores);
+    const sum = scores[0] - scores[2];
+    return sum / count;
+  }
+
+  const getPositive = (scores) => {
+    return scores[0] / getCount(scores) * 100;
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -19,6 +33,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
+      <p>all {getCount([good, neutral, bad])}</p>
+      <p>average {getAverage([good, neutral, bad])}</p>
+      <p>positive {getPositive([good, neutral, bad])} %</p>
     </div>
   )
 };
