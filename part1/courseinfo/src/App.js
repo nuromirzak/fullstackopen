180c6ae1d461bf6f7e969cfd10f383471c278ae0
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-06 20:53:19 +0600

    feat: done exercises 1.4 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,30 +3,31 @@ import Total from "./components/Total";
 import Content from "./components/Content";
 
 const App = () => {
-  const course = "Half Stack application development";
-
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
 
-      <Content
-        parts={[ part1, part2, part3 ]}
-      />
+      <Content parts={course.parts} />
 
-      <Total parts={[ part1, part2, part3 ]} />
+      <Total parts={course.parts} />
     </div>
   );
 };
