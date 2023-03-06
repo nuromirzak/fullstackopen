332dbb74af5eb6583d3af75998ddb97c0ad2928d
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-06 21:50:20 +0600

    feat: done 2.3 - 2.5

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -2,11 +2,11 @@ const Total = (props) => {
   const { parts } = props;
 
   const getSum = (parts) => {
-    const exercises = parts.map((part) => part.exercises);
+    const exercises = parts.map((part) => part.exercises);  
     return exercises.reduce((acc, curr) => acc + curr, 0);
   };
 
-  return <p>Number of exercises {getSum(parts)}</p>;
+  return <p><b>total of {getSum(parts)} exercises</b></p>;
 };
 
 export default Total;
