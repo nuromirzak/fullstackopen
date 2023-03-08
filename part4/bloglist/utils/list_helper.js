367edec7394a318707d6b5ede3234dd711f8dab3
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-08 18:22:24 +0600

    feat: done 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,25 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, currentBlog) => sum + currentBlog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let currMaxLikes = 0, currIndex = 0;
+
+  blogs.forEach((blog, index) => {
+    if (blog.likes > currMaxLikes) {
+      currMaxLikes = blog.likes;
+      currIndex = index;
+    }
+  });
+
+  return {
+    title: blogs[currIndex].title,
+    author: blogs[currIndex].author,
+    likes: blogs[currIndex].likes,
+  };
+};
+
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 };
