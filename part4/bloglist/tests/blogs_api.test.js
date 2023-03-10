c77e918ba5c002720b44a168409c5267531e75c7
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-10 23:24:34 +0600

    feat: done 4.9 - 4.10

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -7,13 +7,24 @@ const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-beforeEach(async () => {
+async function getLengthOfBlogs() {
+  const response = await api.get("/api/blogs");
+
+  return response.body.length;
+}
+
+async function initDatabase() {
   await Blog.deleteMany({});
 
   for (let blog of blogs) {
     let blogObject = new Blog(blog);
+
     await blogObject.save();
   }
+}
+
+beforeEach(async () => {
+  await initDatabase();
 });
 
 test("blogs' length corresponds to the number of blogs in the database and the blogs are returned as json", async () => {
@@ -22,6 +33,33 @@ test("blogs' length corresponds to the number of blogs in the database and the b
     .expect("Content-Type", /application\/json/);
 
   expect(response.body).toHaveLength(blogs.length);
+});
+
+test("the unique identifier property of the blog posts is named _id", async () => {
+  const response = await api.get("/api/blogs");
+
+  const ids = response.body.map((blog) => blog._id);
+
+  for (let id of ids) {
+    expect(id).toBeDefined();
+  }
+});
+
+test("a new blog properly added to the database", async () => {
+  const newBlog = {
+    title: "Testing if a new blog is properly added to the database",
+    author: "OctoCat",
+    url: "https://github.com/octocat",
+    likes: 0,
+  };
+
+  const response = await api.post("/api/blogs").send(newBlog);
+
+  expect(response.body).toMatchObject(newBlog);
+
+  const afterBlogsLength = await getLengthOfBlogs();
+
+  expect(afterBlogsLength).toBe(blogs.length + 1);
 });
 
 afterAll(async () => {
