76ab13f084d7ef0624ede2917e207c122312b747
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-16 19:34:25 +0600

    feat: done 3.7 - 3.8

--- part3/phonebook/app.js
+++ part3/phonebook/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const sample_data = require("./utils/sample_data");
 
 const personsController = require("./controllers/personsController");
 
+morgan.token("body", (req, _) => JSON.stringify(req.body));
+
 app.use(express.json());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 app.use("/api/persons", personsController);
 
