4699183773e57547703cef35f1aef3be5fd66b0b
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-03-28 20:24:25 +0600

    feat: done 7.7 - 7.8

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!name) {
+        return
+      }
+      try {
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        console.log('resonse.data', response.data)
+        setCountry({ found: true, data: response.data[0] })
+      } catch (error) {
+        console.log('error.response', error.response)
+        setCountry({ found: false })
+      }
+    }
+    fetchData()
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name.common}</h3>
+      <div>capital {country.data.capital[0]}</div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`} />
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
---
+++ part7/country-hook/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import App from './App'
+import * as ReactDOMClient from 'react-dom/client';
+
+ReactDOMClient.createRoot(document.getElementById('root')).render(<App />)
