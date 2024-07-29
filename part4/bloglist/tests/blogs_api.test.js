0c14980247d827c70407cefb7b2c459c51ab5d85
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2024-07-29 06:06:26 +0500

    feat: done 4.8

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,107 @@
+const { test, after, beforeEach } = require("node:test");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blog");
+const assert = require("node:assert");
+
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "HTML is easy",
+    author: "John Doe",
+    url: "https://www.example.com",
+    likes: 10,
+  },
+  {
+    title: "CSS is easy",
+    author: "Jane Doe",
+    url: "https://www.example.com",
+    likes: 20,
+  },
+];
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: "willremovethissoon", author: "John Doe" });
+  await blog.save();
+  await blog.remove();
+
+  return blog._id.toString();
+};
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  //   const blogObjects = initialBlogs.map((blog) => new Blog(blog));
+  //   const promiseArray = blogObjects.map((blog) => blog.save());
+  //   await Promise.all(promiseArray);
+  for (let blog of initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+after(() => {
+  mongoose.connection.close();
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("there are two blogs", async () => {
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, initialBlogs.length);
+});
+
+test("the first blog is about HTML", async () => {
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body[0].title, "HTML is easy");
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "CSS is hard",
+    author: "John Doe",
+    url: "https://www.example.com",
+    likes: 0,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((n) => n.title);
+
+  assert.ok(titles.includes("CSS is hard"));
+});
+
+test("blog without title is not added", async () => {
+  const newBlog = {
+    author: "John Doe",
+    url: "https://www.example.com",
+    likes: 0,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
+
+  const blogsAtEnd = await blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, initialBlogs.length);
+});
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 const port = process.env.PORT;
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl =
+  process.env.NODE_ENV === "test"
+    ? "mongodb://nur_user:nur_password@localhost:27018/nur_db?authSource=admin"
+    : process.env.MONGODB_URI;
 
 module.exports = {
   mongoUrl,
